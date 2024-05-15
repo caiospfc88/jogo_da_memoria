6b2f73f7e1353ef6c320c04d6ed5afcc151f7d1c
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-05-15 16:32:45 -0300

    first commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card";
+
+const Board = () => {
+  return (
+    <div className="board">
+      <Card />
+    </div>
+  );
+};
+export default Board;
---
+++ src/components/Game.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Board from "./Board";
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const generateCards = () => {
+  const values = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+  const cards = values.map((value) => ({
+    value,
+    isFlipped: false,
+  }));
+
+  const duplicatedCards = cards
+    .concat([...cards])
+    .map((card, index) => ({ card, index }));
+
+  console.log(shuffleArray(duplicatedCards));
+};
+
+const Game = () => {
+  const [cards, setCards] = useState(generateCards());
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [chances, setChances] = useState(6);
+
+  return (
+    <div className="game">
+      <Board />
+    </div>
+  );
+};
+export default Game;
